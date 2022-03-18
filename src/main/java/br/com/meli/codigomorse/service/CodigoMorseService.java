e5f173a0cf3eb1bab02debe440830f4734a1669e
Maik Henrique Dos Santos Pereira <aipereira@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2022-03-18 11:55:05 -0300

    refactor: extraction to service

---
+++ src/main/java/br/com/meli/codigomorse/service/CodigoMorseService.java
@@ -0,0 +1,96 @@
+package br.com.meli.codigomorse.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class CodigoMorseService {
+    private final static Map<String, String> mapCodigoToCaractere;
+
+    static {
+        mapCodigoToCaractere = new HashMap<>();
+        mapCodigoToCaractere.put(".-", "A");
+        mapCodigoToCaractere.put("-...", "B");
+        mapCodigoToCaractere.put("-.-.", "C");
+        mapCodigoToCaractere.put("-..", "D");
+        mapCodigoToCaractere.put(".", "E");
+        mapCodigoToCaractere.put("..-.", "F");
+        mapCodigoToCaractere.put("--.", "G");
+        mapCodigoToCaractere.put("....", "H");
+        mapCodigoToCaractere.put("..", "I");
+        mapCodigoToCaractere.put(".---", "J");
+        mapCodigoToCaractere.put("-.-", "K");
+        mapCodigoToCaractere.put(".-..", "L");
+        mapCodigoToCaractere.put("--", "M");
+        mapCodigoToCaractere.put("-.", "N");
+        mapCodigoToCaractere.put("---", "O");
+        mapCodigoToCaractere.put(".--.", "P");
+        mapCodigoToCaractere.put("--.-", "Q");
+        mapCodigoToCaractere.put(".-.", "R");
+        mapCodigoToCaractere.put("...", "S");
+        mapCodigoToCaractere.put("-", "T");
+        mapCodigoToCaractere.put("..-", "U");
+        mapCodigoToCaractere.put("...-", "V");
+        mapCodigoToCaractere.put(".--", "W");
+        mapCodigoToCaractere.put("-..-", "X");
+        mapCodigoToCaractere.put("-.--", "Y");
+        mapCodigoToCaractere.put("--..", "Z");
+        mapCodigoToCaractere.put(".----", "1");
+        mapCodigoToCaractere.put("..---", "2");
+        mapCodigoToCaractere.put("...--", "3");
+        mapCodigoToCaractere.put("....-", "4");
+        mapCodigoToCaractere.put(".....", "5");
+        mapCodigoToCaractere.put("-....", "6");
+        mapCodigoToCaractere.put("--...", "7");
+        mapCodigoToCaractere.put("---..", "8");
+        mapCodigoToCaractere.put("----.", "9");
+        mapCodigoToCaractere.put("-----", "0");
+
+        mapCodigoToCaractere.put("..--..", "?");
+        mapCodigoToCaractere.put("-.-.--", "!");
+        mapCodigoToCaractere.put(".-.-.", ".");
+        mapCodigoToCaractere.put("--..--", ".");
+    }
+
+    public String getPalavra(String codigoMorse) {
+        StringBuilder resultado = new StringBuilder();
+        StringBuilder caractereBuilder = new StringBuilder();
+        final int wordSeparatorLength = 3;
+
+        int spaceCounter = 0;
+
+        for (char caractere : codigoMorse.toCharArray()) {
+            System.out.println(spaceCounter);
+
+            if (caractere != ' ') {
+                if (spaceCounter != 0) {
+                    String letra = mapCodigoToCaractere.get(caractereBuilder.toString());
+                    resultado.append(letra);
+
+                    if (spaceCounter == wordSeparatorLength) {
+                        resultado.append(" ");
+                    }
+
+                    spaceCounter = 0;
+                    caractereBuilder.setLength(0);
+
+                }
+                caractereBuilder.append(caractere);
+            }
+
+            if (caractere == ' ') {
+                spaceCounter++;
+            }
+
+        }
+
+        if (caractereBuilder.length() != 0) {
+            String letra = mapCodigoToCaractere.get(caractereBuilder.toString());
+            resultado.append(letra);
+        }
+
+        return resultado.toString();
+    }
+}
